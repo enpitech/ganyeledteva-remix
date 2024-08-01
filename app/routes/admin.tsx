3d417669e663f1809e568fb13317da76a4259827
Nir Parisian <nir.parisian@gmail.com>
2024-08-01 17:26:59 +0300

    fix

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,6 +1,6 @@
-import CMS from "decap-cms-app";
+// import CMS from "decap-cms-app";
 // Initialize the CMS object
-CMS.init();
+// CMS.init();
 // Now the registry is available via the CMS object.
 // CMS.registerPreviewTemplate("my-template", MyTemplate);
 
